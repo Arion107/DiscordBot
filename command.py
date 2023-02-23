5c647e30143ecd3b204a5275e02956986795805d
Arion107 <124314105+Arion107@users.noreply.github.com>
2023-02-23 17:46:15 +0100

    Update command.py

--- command.py
+++ command.py
@@ -1,6 +1,6 @@
 @bot.command()
-async def subscribe(ctx: commands.Context):
-    message = "Open the page below to pick your plan and subscribe!\nhttps://mee6.gg/m/thedream"
+async def info(ctx: commands.Context):
+    message = "test_command"
     await ctx.send(content=message, ephemeral=True)
 
 
