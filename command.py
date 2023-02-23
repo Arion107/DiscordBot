3c2934aa0e181e22fd3120e47d7697653d56f210
Arion107 <124314105+Arion107@users.noreply.github.com>
2023-02-23 18:47:56 +0100

    Update command.py

--- command.py
+++ command.py
@@ -1,6 +1,23 @@
+import os
+import discord
+from discord.ext import commands
+
+async def start_bot():
+    while True:
+        try:
+            bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+            @bot.event
+            async def on_ready():
+                print("Bot has connected to Discord!")
+                bot.loop.create_task(process_queue())
+
 @bot.command()
 async def info(ctx: commands.Context):
     message = "test_command"
     await ctx.send(content=message, ephemeral=True)
 
 
+await bot.start(TOKEN)
+        except Exception as e:
+            print(f"Error: {e}")
+            await bot.close()
